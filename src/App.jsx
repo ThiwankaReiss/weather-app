3553235bef6c79d6de3c7dc69de4248949224276
ThiwankaReiss <thiwankar2003@gmail.com>
2024-02-29 22:22:17 +0530

    Find Weather by typing location complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form'
 import DisplayDetail from './components/displayDetail';
 import './App.css';
-
+import Swal from 'sweetalert2'
 
 function App() {
   const [weatherData, setWeatherData] = useState(null);
-  const searchVal = "Panadura";
+  const [searchVal, setSearchVal] = useState("Panadura");
   const apiKey = "5a3b5d0257934ff585884032242802";
 
-  const { handleSubmit, register } = useForm();
+  const { handleSubmit, register ,formState:{errors} } = useForm();
 
   const submit = (data) => {
     console.log(data);
+
+    setSearchVal(data.city);
   }
 
   useEffect(() => {
@@ -20,8 +22,25 @@ function App() {
       try {
         const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${searchVal}`);
         const data = await response.json();
-        setWeatherData(data);
+        console.log(data)
+//         error: {code: 1006, message: 'No matching location found.'
+
+        if(data.error && data.error.code== 1006){
+          console.log("No matching location found.");
+          
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "No matching location found.",
+            footer: 'Enter Valid Location'
+          });
+          document.getElementById('city').value="";
+        }else{
+          setWeatherData(data);
+        }
+        
       } catch (error) {
+        
         console.error("Error fetching weather data:", error);
       }
     };
@@ -33,19 +52,24 @@ function App() {
     <div className='container-fluid'>
       <div className='row backImage'>
         <div className='col-lg-9'>
-          <div class="col-md-6 p-lg-5 mx-auto my-5">
-            <h1 class="display-3 fw-bold text-warning">Weather App</h1>
-            <h3 class="fw-normal text-info mb-3">Enter Your City Name ?</h3>
+          <div className="col-md-6 p-lg-5 mx-auto my-5">
+            <h1 className="display-3 fw-bold text-warning">Weather App</h1>
+            <h3 className="fw-normal text-info mb-3">Enter Your City Name ?</h3>
 
 
             <div className="container">
               <div className="row">
                 <div className="input-group has-validation col-lg-12">
-                  <span className="input-group-text"><i class="bi bi-globe-americas"></i></span>
-                  <input type="text" {...register("email")} class="form-control" id="username" placeholder="City/Town/Village" required>
+                  <span className="input-group-text"><i className="bi bi-globe-americas"></i></span>
+                  <input type="text" {...register("city",{required:true})} className="form-control" id="city" placeholder="City/Town/Village" required>
                   </input>
-
+                  { errors.city &&  <div className='col-lg-12 text-danger'> <span> City Not Provided &#x2191; 	&#129300;</span> </div>}
                 </div>
+               
+               
+                
+                
+                
                 <div className='col-lg-6'>
                 </div>
                 <div className='col-lg-6 mt-2'>
@@ -58,7 +82,7 @@ function App() {
 
           </div>
         </div>
-        <div className='col-lg-3'>
+        <div className='col-lg-3 outer-container'>
           {weatherData && (
             <DisplayDetail
               cityName={weatherData.location.name}
