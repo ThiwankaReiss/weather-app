4e3dc971611ad93c25846b9735e5c30013fc70e5
ThiwankaReiss <thiwankar2003@gmail.com>
2024-02-29 16:13:07 +0530

    Get weather data from api not await error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import DisplayDetail from './components/displayDetail'
-
-const searchVal="Panadura";
-let dataArray="";
-const apiKey ="5a3b5d0257934ff585884032242802";
-
-    let reop={
-        methord:'GET'
-    };
-
-    fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${searchVal}`,reop)
-    .then(responce=> responce.json())
-    .then(data => {
-        console.log(data);
-
-        // document.getElementById("tempLbl").innerHTML=data["current"]["temp_c"]+"C";
-        // document.getElementById("textLbl").innerHTML=data["current"]["condition"]["text"];
-        dataArray=data;
-
-        // document.getElementById("img").src="https://"+data["current"]["condition"]["icon"];
-        
-    })
-    .then(error => console.log("error",error))
-
+import React, { useState, useEffect } from 'react';
+import DisplayDetail from './components/displayDetail';
+import './App.css';
 
 function App() {
+  const [weatherData, setWeatherData] = useState(null);
+  const searchVal = "Panadura";
+  const apiKey = "5a3b5d0257934ff585884032242802";
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${searchVal}`);
+        const data = await response.json();
+        setWeatherData(data);
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
+      }
+    };
+
+    fetchData();
+  }, [apiKey, searchVal]);
 
   return (
     <div>
-      <DisplayDetail  
-        cityName={dataArray.location.name} 
-        countryName={dataArray.location.country} 
-        region={dataArray.location.region}
-        temeperatureC={dataArray.current.temp_c}   
-        temeperatureF={dataArray.current.temp_f} 
-        weatherCondition={dataArray.current.condition.text} 
-        windSpeedKph={dataArray.current.wind_kph} 
-        windSpeedMph={dataArray.current.wind_mph} 
-        humidity={dataArray.current.humidity}
-        image={dataArray.current.condition.icon}
-      ></DisplayDetail>
+      {weatherData && (
+        <DisplayDetail
+          cityName={weatherData.location.name}
+          countryName={weatherData.location.country}
+          region={weatherData.location.region}
+          temeperatureC={weatherData.current.temp_c}
+          temeperatureF={weatherData.current.temp_f}
+          weatherCondition={weatherData.current.condition.text}
+          windSpeedKph={weatherData.current.wind_kph}
+          windSpeedMph={weatherData.current.wind_mph}
+          humidity={weatherData.current.humidity}
+          image={weatherData.current.condition.icon}
+        />
+      )}
     </div>
-  )
+  );
 }
 
-
-export default App
+export default App;
